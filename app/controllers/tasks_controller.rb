124c83c8598d2275827a46ac829fe9ebccc31dd1
yoshikawa-mikako <yoshikawa-mikako@outlook.jp>
2016-08-20 15:33:35 +0900

    日本語化、背景

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,7 +28,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.html { redirect_to @task, notice: '登録完了しました。' }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to @task, notice: '変更完了しました。' }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit }
@@ -70,7 +70,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.html { redirect_to tasks_url, notice: '削除完了しました。' }
       format.json { head :no_content }
     end
   end
